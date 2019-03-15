cee190b6f2959ace5bcc8c8625fcd74fb513d153
hdsafhb <48586483+hdsafhb@users.noreply.github.com>
2019-03-15 12:45:30 +0100

    test2

--- wapk/wapk/Properties/AssemblyInfo.cs
+++ wapk/wapk/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello_World")]
+[assembly: AssemblyTitle("wapk")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hello_World")]
+[assembly: AssemblyProduct("wapk")]
 [assembly: AssemblyCopyright("Copyright 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
